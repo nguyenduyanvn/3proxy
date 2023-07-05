bb1d33d82c3c77a9da8991cdfe6579e563f10a57
nguyenduyanvn <134839627+nguyenduyanvn@users.noreply.github.com>
2023-07-05 22:07:28 +0700

    Update proxynopass.sh

--- proxynopass.sh
+++ proxynopass.sh
@@ -89,8 +89,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=22001
-LAST_PORT=24000
+FIRST_PORT=1001
+LAST_PORT=1500
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
