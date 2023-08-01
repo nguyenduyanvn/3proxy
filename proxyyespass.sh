3146527f9889827808c7190fd8b76d3522e99fd3
nguyenduyanvn <134839627+nguyenduyanvn@users.noreply.github.com>
2023-08-01 11:37:30 +0700

    Update proxyyespass.sh

--- proxyyespass.sh
+++ proxyyespass.sh
@@ -61,7 +61,7 @@ EOF
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "ipduyanvnAnhquyen123$port/$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "nda$port/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
