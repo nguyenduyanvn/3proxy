3a2ad4d2b877e87882e88ec1b134d5f9efd0a064
nguyenduyanvn <134839627+nguyenduyanvn@users.noreply.github.com>
2023-08-20 00:01:32 +0700

    Update proxyyespass.sh

--- proxyyespass.sh
+++ proxyyespass.sh
@@ -61,7 +61,7 @@ EOF
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "muaxu$port/$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "proxyhcm$port/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
