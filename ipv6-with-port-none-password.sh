83b3dc4f71568d108cfdd1c6f4450117508131f6
nguyenduyanvn <134839627+nguyenduyanvn@users.noreply.github.com>
2023-11-11 12:33:06 +0700

    Update ipv6-with-port-none-password.sh

--- ipv6-with-port-none-password.sh
+++ ipv6-with-port-none-password.sh
@@ -104,7 +104,7 @@ while :; do
     echo "Number out of range, try again"
   fi
 done
-LAST_PORT=$(($FIRST_PORT + 2000))
+LAST_PORT=$(($FIRST_PORT + 500))
 echo "LAST_PORT is $LAST_PORT. Continue..."
 
 gen_data >$WORKDIR/data.txt
