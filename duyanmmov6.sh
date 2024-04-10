d3d145260045a13e0f1aeed779b7a63a0751ae8e
nguyenduyanvn <134839627+nguyenduyanvn@users.noreply.github.com>
2024-04-10 20:01:05 +0700

    Update duyanmmov6.sh

--- duyanmmov6.sh
+++ duyanmmov6.sh
@@ -101,10 +101,15 @@ cat << EOF > /etc/rc.d/rc.local
 touch /var/lock/subsys/local
 EOF
 
+#!/bin/sh
+
 echo "installing apps"
 yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
-install_3proxy
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
 
 echo "working folder = /home/duyanmmo"
 WORKDIR="/home/duyanmmo"
@@ -114,7 +119,7 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
 while :; do
   read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
@@ -126,7 +131,9 @@ while :; do
     echo "Number out of range, try again"
   fi
 done
-LAST_PORT=$(($FIRST_PORT + 999))
+
+read -p "Enter the quantity of ports you want to create: " QUANTITY_OF_PORTS
+LAST_PORT=$(($FIRST_PORT + $QUANTITY_OF_PORTS - 1))
 echo "LAST_PORT is $LAST_PORT. Continue..."
 
 gen_data >$WORKDIR/data.txt
@@ -142,6 +149,7 @@ bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 1000048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
+
 chmod 0755 /etc/rc.local
 bash /etc/rc.local
 
